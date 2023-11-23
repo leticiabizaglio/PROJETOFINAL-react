564d4669655f9c21a3a476da9cb8de61fd69abae
isarmendes <isadoramendes@aluno.senai.br>
2023-11-23 14:37:54 -0300

    feat: criação de pastas

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import { FaEnvelope } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaUsers } from 'react-icons/fa';
+const Footer = () => {
+    return(
+        <>
+        <div>
+        <FaEnvelope />
+        <FaGithub />
+        <FaUsers />
+        </div>
+        <div>
+            <p>©Todos os Direitos Reservados</p>
+        </div>
+        </>
+    )
+}
