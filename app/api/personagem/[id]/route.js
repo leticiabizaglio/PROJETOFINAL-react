5b4ede1e41a498bf5ec08ae928fe85cfab4d31c4
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2023-11-28 16:35:54 -0300

    atualização

--- app/api/personagem/[id]/route.js
+++ app/api/personagem/[id]/route.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { NextResponse } from "next/server";
 
-const url = process.env.BASE_URL + "personagens";
+const url = process.env.BASE_URL + "/personagens";
 
 export async function GET(request, { params }) {
   const { id } = params;
---
+++ app/sobrenos/page.jsx
@@ -0,0 +1,96 @@
+"use client"
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+
+export default function Sobrenos(){
+    const [sobrenos, setSobrenos] = useState([]);
+        const [ dados, setDados] = useState([]);
+        const router = useRouter();
+
+
+        const deletar = async (id) => {
+            const url = `/api/sobrenos/${id}`;
+            try {
+                await axios.delete(url);
+                setSobrenos(sobrenos.filter((sobrenos) => sobrenos.id !== id));
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+    
+        const update = async (id) => {
+            router.push(`/sobrenos/${id}`);
+        };
+    
+        useEffect(() => {
+            async function fetchSobrenos() {
+                try {
+                    const response = await axios.get("/api/sobrenos");
+                    setSobrenos(response.data.data);
+                    setDados(response.data.data);
+                } catch (error) {
+                    console.error("Error fetching data:", error);
+                }
+            }
+            fetchSobrenos();
+        },[]);
+    
+        return(
+            <div>
+                <h1>HARRY POTTER</h1>
+                <div>
+                    <h1>Sobre Nós</h1>
+    
+                    {dados.length ? (
+                        <div>
+                        {sobrenos.map((sobrenos) => (
+                            <div key={sobrenos.id} >
+                            <div>
+                                <p>
+                                    <strong>ID:</strong> {sobrenos.id}
+                                </p>
+                                <p>
+                                    <strong>Nome:</strong> {sobrenos.nome}
+                                </p>
+                                <p>
+                                    <strong>Imagem:</strong> {sobrenos.imagem}
+                                </p>
+                                <p>
+                                    <strong>Idade:</strong> {sobrenos.idade}
+                                </p>
+                                <p>
+                                    <strong>Escola:</strong> {sobrenos.escola}
+                                </p>
+                                <p>
+                                    <strong>Casa:</strong> {sobrenos.casa}
+                                </p>
+                                <p>
+                                    <strong>Email:</strong> {sobrenos.email}
+                                </p>
+                            </div>
+    
+                                    <div >
+                                        <button
+                                            onClick={() => deletar(sobrenos.id)}
+                                        >
+                                            <FaTrash /> Deletar
+                                        </button>
+                                        <button
+                                            onClick={() => update(sobrenos.id)}
+                                        >
+                                            <FaEdit /> Atualizar
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>Carregado...</p>
+                    )}
+                </div>
+            </div>
+        )
+    }
