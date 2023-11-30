deee8b5dfde82e766baf894f54328b58334d59d9
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-30 10:16:21 -0300

    feat: add page.jsx pahe without api info

---
+++ app/sobreHP/page.jsx
@@ -0,0 +1,25 @@
+"use client"
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
+export default function SobreHP(){
+        return(
+            <div>
+                <h1>HARRY POTTER</h1>
+                <div>
+                    <h1>Sobre HP</h1>
+    
+                        <div>
+                            <h1>Hogwarts Legacy</h1>
+                            <p>b</p>
+                            <h1>Filmes</h1>
+                            <p>a</p>
+                            <h1>Livros</h1>
+                            <p>c</p>                           
+                        </div>
+                </div>
+            </div>
+        )
+    }
