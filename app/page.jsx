272320fe9518cef06a974e862952b09618679b85
JuliaFee <julia.silva133@aluno.senai.br>
2023-12-05 16:27:15 -0300

    feat: start index components

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,34 @@
-"use client"
+"use client";
 
-import Footer from "./components/footer/Footer"
-import Header from "./components/header/Header"
-import PopUp from "./components/popUp/PopUp"
+import Link from "next/link";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import style from "./page.module.css";
+// import PopUp from "./components/popUp/PopUp"
 
 export default function Home() {
   return (
-    <>
-    <Header></Header>
-    <h1>HARRY POTTER</h1>
-    <PopUp />
-    <Footer></Footer>
-    </>
-  )
+    <div>
+      <Header></Header>
+      <h1>HARRY POTTER</h1>
+      <div style={style.container}>
+        <div className={style.section1}>
+          <div className={style.bg}>{/* imagem hogwarts Legacy */}</div>
+          <h1>Você já conferiu</h1>
+          <Link href="https://www.hogwartslegacy.com/pt-br">
+            Hogwarts Legacy
+          </Link>
+        </div>
+        <br></br>
+        <div className={style.section2}>
+          <div className={style.bg}>{/* imagem aleatoria harry potter */}</div>
+          <h1>E o novo documentario?</h1>
+          <Link href="https://www.google.com/search?client=opera-gx&q=documentario+hbo+max+harry+potter&sourceid=opera&ie=UTF-8&oe=UTF-8&si=ALGXSlZZLz93Q5j8HVkpXyxpTaoqXw8cocmoi-DFAGsSj5diF-MUY30eVVhwmxY_ZSnPuRCwzAeuaxVouSu1mw5OiIXbs2XcWIOtcTdUIZSsbcqCAR3pEaeE08pS5v7Actv9DQbeNa4l_CPUHK1w1TECrdMOoC0SvFV_QavEBtD0bfxyVTO22PI%3D&ictx=1&ved=2ahUKEwi2lcKIgvmCAxW7p5UCHbtICXEQ_coHegQINRAD">
+            Comemoração 20 anos de Harry Potter
+          </Link>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
