858f16fdd16234f024ebf11d9fd016d28dca0471
JuliaFee <julia.silva133@aluno.senai.br>
2023-12-07 16:47:54 -0300

    feat: input page

--- app/page.jsx
+++ app/page.jsx
@@ -15,8 +15,8 @@ export default function Home() {
       <div className={style.container}>
       <h1>HARRY POTTER</h1>
         <div className={style.section1}>
-        <h1>Você já conferiu</h1>
-          <Link href="https://www.hogwartslegacy.com/pt-br">
+        <h1 className={style.title}>Você já conferiu</h1>
+          <Link className={style.link} href="https://www.hogwartslegacy.com/pt-br">
             Hogwarts Legacy
           </Link>
           <div className={style.bg1}>{/* imagem hogwarts Legacy */}</div>
@@ -26,8 +26,8 @@ export default function Home() {
 
         <div className={style.section2}>
           <div className={style.bg2}>{/* imagem aleatoria harry potter */}</div>
-          <h1>E o novo documentario?</h1>
-          <Link href="https://www.google.com/search?client=opera-gx&q=documentario+hbo+max+harry+potter&sourceid=opera&ie=UTF-8&oe=UTF-8&si=ALGXSlZZLz93Q5j8HVkpXyxpTaoqXw8cocmoi-DFAGsSj5diF-MUY30eVVhwmxY_ZSnPuRCwzAeuaxVouSu1mw5OiIXbs2XcWIOtcTdUIZSsbcqCAR3pEaeE08pS5v7Actv9DQbeNa4l_CPUHK1w1TECrdMOoC0SvFV_QavEBtD0bfxyVTO22PI%3D&ictx=1&ved=2ahUKEwi2lcKIgvmCAxW7p5UCHbtICXEQ_coHegQINRAD">
+          <h1 className={style.title}>E o novo documentario?</h1>
+          <Link className={style.link} href="https://www.google.com/search?client=opera-gx&q=documentario+hbo+max+harry+potter&sourceid=opera&ie=UTF-8&oe=UTF-8&si=ALGXSlZZLz93Q5j8HVkpXyxpTaoqXw8cocmoi-DFAGsSj5diF-MUY30eVVhwmxY_ZSnPuRCwzAeuaxVouSu1mw5OiIXbs2XcWIOtcTdUIZSsbcqCAR3pEaeE08pS5v7Actv9DQbeNa4l_CPUHK1w1TECrdMOoC0SvFV_QavEBtD0bfxyVTO22PI%3D&ictx=1&ved=2ahUKEwi2lcKIgvmCAxW7p5UCHbtICXEQ_coHegQINRAD">
             Comemoração 20 anos de Harry Potter
           </Link>
         </div>
@@ -36,16 +36,16 @@ export default function Home() {
 
         <div className={style.section3}>
           <div className={style.bg3}>{/* imagem aleatoria harry potter */}</div>
-          <h1>Uma visão geral sobre Harry Potter</h1>
-          <Link href="/">Sobre o HP</Link>
+          <h1 className={style.title}>Uma visão geral sobre Harry Potter</h1>
+          <Link className={style.link} href="/">Sobre o HP</Link>
       </div>
 
 <br></br>
 
 <div className={style.section4}>
           <div className={style.bg4}>{/* imagem aleatoria harry potter */}</div>
-          <h1>Faça seu própio Personagem!</h1>
-          <Link href="/">Link para o personagem</Link>
+          <h1 className={style.title}>Faça seu própio Personagem!</h1>
+          <Link className={style.link} href="/">Link para o personagem</Link>
       </div>
 
 
