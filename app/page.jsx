2b0f29027c2c0691c467109476885e94d18adea2
JuliaFee <julia.silva133@aluno.senai.br>
2023-12-07 17:16:44 -0300

    feat: style index

--- app/page.jsx
+++ app/page.jsx
@@ -15,40 +15,54 @@ export default function Home() {
       <div className={style.container}>
       <h1>HARRY POTTER</h1>
         <div className={style.section1}>
-        <h1 className={style.title}>Você já conferiu</h1>
+          <div className={style.bannerContainer}>
+            <div className={style.banner}>
+            <h1 className={style.title}>Você já conferiu</h1>
           <Link className={style.link} href="https://www.hogwartslegacy.com/pt-br">
             Hogwarts Legacy
           </Link>
-          <div className={style.bg1}>{/* imagem hogwarts Legacy */}</div>
+            </div>
+          </div>
         </div>
 
         <br></br>
 
         <div className={style.section2}>
-          <div className={style.bg2}>{/* imagem aleatoria harry potter */}</div>
-          <h1 className={style.title}>E o novo documentario?</h1>
+          <div className={style.bannerContainer}>
+            <div className={style.banner2}>
+            <h1 className={style.title}>Novo HBOMAX doc</h1>
           <Link className={style.link} href="https://www.google.com/search?client=opera-gx&q=documentario+hbo+max+harry+potter&sourceid=opera&ie=UTF-8&oe=UTF-8&si=ALGXSlZZLz93Q5j8HVkpXyxpTaoqXw8cocmoi-DFAGsSj5diF-MUY30eVVhwmxY_ZSnPuRCwzAeuaxVouSu1mw5OiIXbs2XcWIOtcTdUIZSsbcqCAR3pEaeE08pS5v7Actv9DQbeNa4l_CPUHK1w1TECrdMOoC0SvFV_QavEBtD0bfxyVTO22PI%3D&ictx=1&ved=2ahUKEwi2lcKIgvmCAxW7p5UCHbtICXEQ_coHegQINRAD">
-            Comemoração 20 anos de Harry Potter
+           Comemoração de 20 anos HP
           </Link>
-        </div>
+            </div>
+          </div>
+          </div>
 
         <br></br>
 
-        <div className={style.section3}>
-          <div className={style.bg3}>{/* imagem aleatoria harry potter */}</div>
-          <h1 className={style.title}>Uma visão geral sobre Harry Potter</h1>
-          <Link className={style.link} href="/">Sobre o HP</Link>
-      </div>
+        <div className={style.section2}>
+          <div className={style.bannerContainer}>
+            <div className={style.banner3}>
+            <h1 className={style.title}>Faça seu propio personagem</h1>
+          <Link className={style.link} href="/personagem/cadastro-personagem">
+           Registre-se
+          </Link>
+            </div>
+          </div>
+          </div>
 
 <br></br>
 
-<div className={style.section4}>
-          <div className={style.bg4}>{/* imagem aleatoria harry potter */}</div>
-          <h1 className={style.title}>Faça seu própio Personagem!</h1>
-          <Link className={style.link} href="/">Link para o personagem</Link>
-      </div>
-
-
+<div className={style.section2}>
+          <div className={style.bannerContainer}>
+            <div className={style.banner4}>
+            <h1 className={style.title}>Sobre HP</h1>
+          <Link className={style.link} href="/sobreHP">
+            Sobre Harry Potter
+          </Link>
+            </div>
+          </div>
+          </div>
 
       <Footer></Footer>
     </div>
