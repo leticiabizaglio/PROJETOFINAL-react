31ad676775d602d84f38d2d9b8327eabdb2e6421
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2023-12-12 16:46:24 -0300

    feat: css sobrehp

--- app/sobreHP/page.jsx
+++ app/sobreHP/page.jsx
@@ -3,6 +3,7 @@
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import style from "./sobrehp.module.css";
+import Divisory from "../components/divisory/Divisory";
 
 
 export default function SobreHP() {
@@ -11,11 +12,14 @@ export default function SobreHP() {
             <Header />
 
             <div className={style.container}>
-                <h1 className={style.titulo}>HARRY POTTER</h1>
+            <div className={style.mainText}>
+            <p className={style.title}>Sobre os filmes</p>
+            <p className={style.subtitle}>Harry Potter</p>
+          </div>
+          <Divisory/>
                 <div className={style.section1}>
-                    <h1>Sobre os filmes</h1>
                     <div className={style.bg1}>
-                        <img src={"https://cdn.europosters.eu/image/hp/80594.jpg"}></img>
+                        <img className={style.image} src={"https://cdn.europosters.eu/image/hp/80594.jpg"}></img>
                     </div>
                     <div className={style.text}>
                         <p>Baseados na série de livros da autora J.K Rowlings, os filmes do harry potter acompanharam o crescimento de uma geração e conquistaram fãs pelo mundo todo.
@@ -25,82 +29,100 @@ export default function SobreHP() {
                             São oito filmes produzidos pela Warner Bros., que seguem a ordem dos livros e levam o público para uma jornada repleta de feitiços, criaturas mágicas e grandes batalhas. Confira a seguir a ordem cronológica da franquia: </p>
 
                         <br></br>
+                        <Divisory/>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e a Pedra Filosofal (2001):</h2>
-                            <img src={"https://musicart.xboxlive.com/7/5dbd5100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"}></img>
-                            <p>Apresenta um garoto órfão, que descobre ter uma herança mágica e ser sobrevivente de um ataque do Lord Voldemort (Ralph Fiennes), um dos maiores bruxos das trevas.
+                          
+                            <p className={style.nomes}>Harry Potter e a Pedra Filosofal (2001):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://musicart.xboxlive.com/7/5dbd5100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"}></img>
+                            <p className={style.paragrafo}>Apresenta um garoto órfão, que descobre ter uma herança mágica e ser sobrevivente de um ataque do Lord Voldemort (Ralph Fiennes), um dos maiores bruxos das trevas.
                                 A produção é dirigida por Chris Columbus e destaca outros personagens importantes, que acompanham Harry (Daniel Radcliffe) ao longo dos filmes, como Hermione Granger (Emma Watson), Rony Weasley (Rupert Grint) e Hagrid (Robbie Coltrane). </p>
+                        </div>
                         </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e a Câmara Secreta (2002): </h2>
-                            <img src={"https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2022/11/25/unnamed-1ib3894cp3hwm.jpg"}></img>
-                            <p>Um ano após o lançamento do primeiro filme, “Harry Potter e a Câmara Secreta” chega aos cinemas, com uma visão mais aprofundada de Hogwarts e seus segredos.
+                       
+                            <p className={style.nomes}>Harry Potter e a Câmara Secreta (2002): </p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2022/11/25/unnamed-1ib3894cp3hwm.jpg"}></img>
+                            <p  className={style.paragrafo}>Um ano após o lançamento do primeiro filme, “Harry Potter e a Câmara Secreta” chega aos cinemas, com uma visão mais aprofundada de Hogwarts e seus segredos.
                                 A história apresenta novos personagens, como o elfo doméstico Dobby, e acompanha a aventura de Harry Potter e seus amigos para combater um mal que assombra a escola.
                                 Juntos, eles desvendam os mistérios da Câmara Secreta e estreitam os laços de amizade ao longo de mais um ano em Hogwarts. </p>
                         </div>
+                        </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e o Prisioneiro de Azkaban (2004):</h2>
-                            <img src={"https://rollingstone.uol.com.br/media/uploads/harry-potter-e-o-prisioneiro-de-azkaban_reproducao_twitter.jpg"}></img>
-                            <p>O jovem bruxo volta às telonas dois anos depois com “Harry Potter e o Prisioneiro de Azkaban”. A produção traz um cenário mais sombrio ao universo de Hogwarts, com os dementadores e situações mais desafiadoras para os personagens.
+                            <p className={style.nomes}>Harry Potter e o Prisioneiro de Azkaban (2004):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://img.elo7.com.br/product/main/2657A05/big-poster-harry-potter-e-o-prisioneiro-de-azkaban-lo2-90x60-geek.jpg"}></img>
+                            <p  className={style.paragrafo}>O jovem bruxo volta às telonas dois anos depois com “Harry Potter e o Prisioneiro de Azkaban”. A produção traz um cenário mais sombrio ao universo de Hogwarts, com os dementadores e situações mais desafiadoras para os personagens.
                                 Com direção de Alfonso Cuarón, essa sequência dos filmes do Harry Potter conta a história de Sirius Black (Gary Oldman), prisioneiro que escapou de Azkaban e tem uma forte relação com Potter.
                                 Ao longo do filme, o público pode conhecer mais sobre o passado de Harry e sua família, além de entender outros detalhes do ataque de Lord Voldemort. </p>
                         </div>
+                        </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e o Cálice de Fogo (2005):</h2>
-                            <img src={"https://memorial.org.br/wp-content/uploads/2020/10/FILMES_Prancheta-1-c%C3%B3pia-3.png"}></img>
-                            <p>Lançado em 2005, “Harry Potter e o Cálice de Fogo” traz uma competição para a escola de Hogwarts: o Torneio Tribruxo.
+                            <p className={style.nomes}>Harry Potter e o Cálice de Fogo (2005):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://memorial.org.br/wp-content/uploads/2020/10/FILMES_Prancheta-1-c%C3%B3pia-3.png"}></img>
+                            <p  className={style.paragrafo}>Lançado em 2005, “Harry Potter e o Cálice de Fogo” traz uma competição para a escola de Hogwarts: o Torneio Tribruxo.
                                 Harry é selecionado para o torneio de forma inesperada e precisa enfrentar os desafios e perigos das provas ao lado de outros três alunos.
                                 Além das aventuras e da presença de Lord Voldemort, o quarto filme da saga também apresenta outras escolas de magia e traz uma narrativa mais madura, que reflete o crescimento dos personagens. </p>
                         </div>
+                        </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e a Ordem da Fênix (2007):</h2>
-                            <img src={"https://www.themoviedb.org/t/p/w500/tIf9aUyNljda9MG1pjlOLHCZ3b0.jpg"}></img>
-                            <p>Em “Harry Potter e a Ordem da Fênix”, o jovem conhecido como “o garoto que sobreviveu” precisa enfrentar a oposição do Ministério da Magia, que não acredita em sua versão dos fatos sobre o retorno de Lord Voldemort.
+                            <p className={style.nomes}>Harry Potter e a Ordem da Fênix (2007):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://www.themoviedb.org/t/p/w500/tIf9aUyNljda9MG1pjlOLHCZ3b0.jpg"}></img>
+                            <p  className={style.paragrafo}>Em “Harry Potter e a Ordem da Fênix”, o jovem conhecido como “o garoto que sobreviveu” precisa enfrentar a oposição do Ministério da Magia, que não acredita em sua versão dos fatos sobre o retorno de Lord Voldemort.
                                 Após o Torneio Tribruxo, Harry é questionado pelo ministro e pelos colegas sobre o duelo com o Lord das Trevas, enquanto se prepara para tempos difíceis.
                                 A produção, dirigida por David Yates e lançada em 2007, foca nesse conflito e apresenta a Ordem da Fênix, um grupo de bruxos que lutou contra Voldemort no passado e se reúne novamente para lidar com a ameaça iminente. </p>
                         </div>
+                        </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e o Enigma do Príncipe (2009):</h2>
-                            <img src={"https://i.ytimg.com/vi/sG-LDd5IUAk/maxresdefault.jpg"}></img>
-                            <p>David Yates volta para dirigir “Harry Potter e o Enigma do Príncipe”, lançado em 2009. O filme explora o passado de Lord Voldemort e revela a sua trajetória antes de se tornar um bruxo das trevas.
+                            <p className={style.nomes}>Harry Potter e o Enigma do Príncipe (2009):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://i.ytimg.com/vi/sG-LDd5IUAk/maxresdefault.jpg"}></img>
+                            <p  className={style.paragrafo}>David Yates volta para dirigir “Harry Potter e o Enigma do Príncipe”, lançado em 2009. O filme explora o passado de Lord Voldemort e revela a sua trajetória antes de se tornar um bruxo das trevas.
                                 Nessa produção, a história assume um ar mais sombrio, reflexo da guerra iminente, do amadurecimento e das perdas enfrentadas pelos personagens.
                                 Esse capítulo da saga de Harry Potter também mostra detalhes importantes que são essenciais para derrotar Voldemort. </p>
                         </div>
-
+                        </div>
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e as Relíquias da Morte: Parte 1 (2010):</h2>
-                            <img src={"https://i.ytimg.com/vi/A4YqYV0kE48/maxresdefault.jpg"}></img>
-                            <p>Para concluir a lista de filmes do Harry Potter, o último livro, intitulado “Harry Potter e as Relíquias da Morte” foi dividido em duas partes.
+                            <p className={style.nomes}>Harry Potter e as Relíquias da Morte: Parte 1 (2010):</p>
+                            <div className={style.card}>
+                            <img className={style.img} src={"https://i.ytimg.com/vi/A4YqYV0kE48/maxresdefault.jpg"}></img>
+                            <p  className={style.paragrafo}>Para concluir a lista de filmes do Harry Potter, o último livro, intitulado “Harry Potter e as Relíquias da Morte” foi dividido em duas partes.
                                 A primeira foi lançada em 2010, também com direção de David Yates, e marca o início da aventura de Harry, Rony e Hermione em busca das horcruxes, objetos que guardam pedaços da alma do Lord Voldemort.
                                 Longe dos amigos e das famílias, os personagens partem em uma jornada desafiadora contra o mal e vivem um grande crescimento pessoal. </p>
+                        </div>
                         </div>
 
                         <br></br>
 
                         <div className={style.filme}>
-                            <h2>Harry Potter e as Relíquias da Morte: Parte 2 (2011):</h2>
-                            <img src={"https://cinepop.com.br/wp-content/uploads/2021/07/harry-potter-and-the-deathly-hallows.jpg"}></img>
-                            <p>A conclusão da saga que encanta gerações chega em 2011 com a segunda parte de “Harry Potter e as Relíquias da Morte”.
+                            <p className={style.nomes}>Harry Potter e as Relíquias da Morte: Parte 2 (2011):</p>
+                            <div className={style.card}>
+                            <img  className={style.img} src={"https://cinepop.com.br/wp-content/uploads/2021/07/harry-potter-and-the-deathly-hallows.jpg"}></img>
+                            <p  className={style.paragrafo}>A conclusão da saga que encanta gerações chega em 2011 com a segunda parte de “Harry Potter e as Relíquias da Morte”.
                                 Sob a direção de David Yates, a última produção da lista de filmes do Harry Potter apresenta o confronto final entre o jovem bruxo e o Lord das Trevas, que leva a uma grande batalha em Hogwarts.
                                 A despedida de Hogwarts é repleta de emoções, sacrifícios e cenas grandiosas, que marcaram os fãs e o universo cinematográfico – com direito a uma espiada no futuro dos personagens para fechar a história. </p>
+                        </div>
                         </div>
 
                         <br></br>
@@ -109,25 +131,29 @@ export default function SobreHP() {
                 </div>
 
                 <br></br>
+                <Divisory/>
 
-                <h1>Sobre Hogwarts Legacy:</h1>
+                <div className={style.mainText}>
+            <p className={style.title}>Sobre o jogo</p>
+            <p className={style.subtitle}>Hogwarts Legacy</p>
+          </div>
                 <div className={style.bg1}>
-                    <img src={"https://t.ctcdn.com.br/1sowh9w23vU0hLhvjjDJQm_yMbk=/1200x675/smart/i574923.jpeg"}></img>
+                    <img className={style.img2} src={"https://t.ctcdn.com.br/1sowh9w23vU0hLhvjjDJQm_yMbk=/1200x675/smart/i574923.jpeg"}></img>
                 </div>
                 <div className={style.text}>
-                    <p>O jogo Hogwarts Legacy é um RPG muito famoso e conhecido de Harry Potter que envolve ação imersiva e de mundo aberto ambientado no mundo bruxo dos anos 1800, colocando os jogadores no centro de suas próprias aventuras.</p>
-                    <p>As plataformas onde o jogo está disponível são os consoles PlayStation®5, PlayStation®4, PlayStation®4 Pro, Xbox Series X|S, Xbox One, Nintendo Switch e PC. </p>
-                    <p>É um jogo muito interativo pois à medida que você vai desenvolvendo relacionamentos com os jogadores, esses colegas de escola se tornarão companheiros que podem acompanhar os jogadores em sua jornada, expandir suas habilidades e se abrir conforme os jogadores aprendem suas histórias.</p>
+                    <p className={style.paragrafo2}>O jogo Hogwarts Legacy é um RPG muito famoso e conhecido de Harry Potter que envolve ação imersiva e de mundo aberto ambientado no mundo bruxo dos anos 1800, colocando os jogadores no centro de suas próprias aventuras.</p>
+                    <p className={style.paragrafo2}>As plataformas onde o jogo está disponível são os consoles PlayStation®5, PlayStation®4, PlayStation®4 Pro, Xbox Series X|S, Xbox One, Nintendo Switch e PC. </p>
+                    <p className={style.paragrafo2}>É um jogo muito interativo pois à medida que você vai desenvolvendo relacionamentos com os jogadores, esses colegas de escola se tornarão companheiros que podem acompanhar os jogadores em sua jornada, expandir suas habilidades e se abrir conforme os jogadores aprendem suas histórias.</p>
                     <br></br>
                     <img src={"https://uploads.jovemnerd.com.br/wp-content/uploads/2022/11/hogwarts_legacy_gameplay_30_minutos__kap175-1210x544.jpg"}></img>
                     <br></br>
-                    <p>Os jogadores podem vincular sua conta do fã clube de Harry Potter com sua conta WB Games com base na experiência de seleção oficial do fã clube de Harry Potter, juntamente com a cerimônia de varinhas, os jogadores podem trazer seu eu autêntico para o jogo. A vinculação de contas também desbloqueará uma máscara de caveira com bico e vestes exclusivas de fãs da casa no jogo, um conjunto especial de vestes temáticas das casas para que os jogadores possam representar sua casa de Hogwarts com estilo. </p>
-                    <p>E vale lembra que personagens como Harry, Hermione ou Draco não aparecem em Hogwarts Legacy  já que o jogo se passa nos anos 1800, antes do período das histórias originais de J.K. Rowling. </p>
-                    <p>Além de classes e feitiços, os jogadores terão acesso a um conjunto grande de melhorias, talentos e habilidades para ajudar na progressão como bruxa ou bruxo. Eles podem completar desafios em todo o mundo para ganhar experiência e aumentar suas habilidades. Equipamentos mágicos também podem ser atualizados e especializados para aumentar as capacidades ofensivas e defensivas no seu estilo de jogo escolhido. </p>
+                    <p className={style.paragrafo2}>Os jogadores podem vincular sua conta do fã clube de Harry Potter com sua conta WB Games com base na experiência de seleção oficial do fã clube de Harry Potter, juntamente com a cerimônia de varinhas, os jogadores podem trazer seu eu autêntico para o jogo. A vinculação de contas também desbloqueará uma máscara de caveira com bico e vestes exclusivas de fãs da casa no jogo, um conjunto especial de vestes temáticas das casas para que os jogadores possam representar sua casa de Hogwarts com estilo. </p>
+                    <p className={style.paragrafo2}>E vale lembra que personagens como Harry, Hermione ou Draco não aparecem em Hogwarts Legacy  já que o jogo se passa nos anos 1800, antes do período das histórias originais de J.K. Rowling. </p>
+                    <p className={style.paragrafo2}>Além de classes e feitiços, os jogadores terão acesso a um conjunto grande de melhorias, talentos e habilidades para ajudar na progressão como bruxa ou bruxo. Eles podem completar desafios em todo o mundo para ganhar experiência e aumentar suas habilidades. Equipamentos mágicos também podem ser atualizados e especializados para aumentar as capacidades ofensivas e defensivas no seu estilo de jogo escolhido. </p>
 
                 </div>
 
-
+                <Divisory/>
 
                 <Footer />
             </div>
