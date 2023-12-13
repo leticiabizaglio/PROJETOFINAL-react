6e3cd0869177d390a60ad4d1b1523403b8925f1e
maria eduarda <123502092+Mariavalonga@users.noreply.github.com>
2023-12-13 15:40:33 -0300

    feat: alterações

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -4,54 +4,31 @@ import { FaEnvelope } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaUsers } from 'react-icons/fa';
 import style from "./footer.module.css";
-
+import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className={style.global}>
-            <div className={style.footercontent}>
-                <div className={style.footercontact}>
-
-                    <Link href={"/"} className={style.home}>
-                        <img src="https://see.fontimg.com/api/renderfont4/MVZ6w/eyJyIjoiZnMiLCJoIjo2NiwidyI6MTI1MCwiZnMiOjUzLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiI0Y2RjVGNSIsInQiOjF9/SGFycnkgUG90dGVy/harry-p.png" className={style.hp}></img>
-                    </Link>
-                    <a href='#' className={style.footerlink}>
-                        <a className={style.footericons1}>
-                            <FaEnvelope />
-                        </a >
-                        <a className={style.footericons2}>
-                            <FaGithub />
-                        </a>
-                        <a className={style.footericons3}>
-                            <FaUsers />
-                        </a>
-                    </a>
-                    {/* <p className={style.always}>after all this time? always</p> */}
-                    <div className={style.footersocialmedia}>
-
-                        <div className={style.footerlist}>
-                            <h3 className={style.pgs}>Páginas</h3>
-
-                            <Link href={"/sobrenos"} className={style.sobrenos}>Sobre nós</Link>
-                            <Link href={"/contato"} className={style.contato}>Contato</Link>
-                            <Link href={"/sobreHP"} className={style.sobrehp}>Sobre HP</Link>
-
-                        </div>
-
-
+            <div className={style.container}>
+                <a href='#'><FaInstagram /></a>
+                <a href='#'><FaEnvelope /></a>
+                <a href='#'><FaGithub /></a>
+                <a href='#'><FaUsers /></a>
+            </div>
+            <div className={style.footer}>
+                <ul className={style.ul}>
+                    
+                        <Link href="/sobrenos" className={style.li}>
+                            Sobre Nós</Link>
                    
-
-                    </div>
-                    <div>
-                    </div>
-                </div>
-                <div className={style.reservados}>
-                <p className={style.direitos}>©Todos os Direitos Reservados</p>
-                </div>
-
-               
+                        <Link href="/contato" className={style.li}>
+                            Contato</Link>
+                  
+                </ul>
+            </div>
+            <div className={style.escrito}>
+                <p>CopyRight ©Todos os Direitos Reservados - 2023 <span className={style.span}>By Azuno</span></p>
             </div>
         </div>
-
     )
 }
 export default Footer;
